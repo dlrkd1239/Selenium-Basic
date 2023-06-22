6a7ded219cd48d28902c8e8b553289c5fa9cb7eb
harigaze <dlrkd1122@gmail.com>
2023-06-22 17:07:55 +0900

    Udemy Selenium Project.
    Initial Commit

---
+++ main.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+
+chrome_driver_path = 'C:/Users/dlrkd/Desktop/chromedriver_win32/chromedriver.exe'
+driver = webdriver.Chrome()
+
+driver.get("https://www.naver.com")
