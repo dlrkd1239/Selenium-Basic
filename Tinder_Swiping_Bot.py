3abd62a05fa1f88b1cc44f3c3f9cf28dc8274030
harigaze <dlrkd1122@gmail.com>
2023-06-29 09:23:04 +0900

    Udemy Selenium Project
    Tinder Auto-Swiping Bot
    
    ADD: Now bot press the "NOPE" button.

--- Tinder_Swiping_Bot.py
+++ Tinder_Swiping_Bot.py
@@ -12,7 +12,7 @@ time.sleep(3)
 def login():
     driver.find_elements(By.CLASS_NAME, "w1u9t036")[1].click()
     time.sleep(2)
-    driver.find_element(By.XPATH, '//*[@id="q1595321969"]/main/div[1]/div/div[1]/div/div/div[2]/div[2]/span/div[2]/button/div[2]/div[2]/div/div').click()
+    driver.find_element(By.XPATH, '//*[@id="u-1238065328"]/main/div[1]/div/div[1]/div/div/div[2]/div[2]/span/div[2]/button').click()
     time.sleep(2)
     driver.switch_to.window(driver.window_handles[1])
     driver.find_element(By.NAME, "email").send_keys(environ.get("username"))
@@ -21,11 +21,16 @@ def login():
     time.sleep(10)
 
 login()
-driver.find_element(By.XPATH, '//*[@id="q1595321969"]/main/div[1]/div/div/div[3]/button[1]').click()
+driver.find_element(By.XPATH, '//*[@id="u-1238065328"]/main/div[1]/div/div/div[3]/button[1]').click()
 time.sleep(1)
-driver.find_element(By.XPATH, '//*[@id="q1595321969"]/main/div[1]/div/div/div[3]/button[1]').click()
+driver.find_element(By.XPATH, '//*[@id="u-1238065328"]/main/div[1]/div/div/div[3]/button[1]').click()
 time.sleep(10)
 
+buttons = driver.find_elements(By.TAG_NAME, 'button')
+for item in buttons:
+    if item.text == "NOPE":
+        nope = item
+
 while True:
-    #driver.find_element(By.TAG_NAME, 'button').click()
-    time.sleep(2)
+    nope.send_keys(Keys.ENTER)
+    time.sleep(3)
